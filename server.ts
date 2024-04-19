e56498e9dcd076a41958e6c6c38295cac091ba85
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2024-04-19 16:06:23 +0530

    Add zod npm dependency and update server.ts

--- server.ts
+++ server.ts
@@ -1,14 +1,14 @@
 import app from "./src/app";
 import { env_variable } from "./src/config";
 import connectDB from "./src/connection";
+import chalk from "chalk";
 
 const PORT: number = env_variable.PORT
 const MONGO_DB_BASE_URL: string = env_variable.MONGO_DB_BASE_URL as string
 const COLLECTION_NAME: string = 'myapp'
 
-app.listen(PORT, () => {     
-  connectDB(`${MONGO_DB_BASE_URL}/${COLLECTION_NAME}`)
-  console.log(`Server it started http://localhost:${PORT}`)
+app.listen(PORT, async () : Promise<void> => {     
+  await connectDB(`${MONGO_DB_BASE_URL}/${COLLECTION_NAME}`)
+  console.log(chalk.green(`Server it started http://localhost:${PORT}`))
 })  
-
-console.log('Hello World!')  
+    
